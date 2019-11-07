da711c6a0a9484583ddfe7e1fdafa8867415cb13
Jaron Wilding <jaronwilding95@gmail.com>
2019-11-07 08:16:31 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float xInput;
+    public float zInput;
+    public bool inputMovement;
+    public float moveSpeed = 2.0f;
+
+    public CharacterController charCon;
+
+    private void Start()
+    {
+        charCon = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        xInput = Input.GetAxis("LHorizontal");
+        zInput = Input.GetAxis("LVertical");
+
+        inputMovement = xInput <= -0.2f || xInput >= 0.2f || zInput <= -0.2f || zInput >= 0.2f ? true : false;
+
+        if (Input.GetButton("LThumbPress")) { moveSpeed = 4f; } else { moveSpeed = 2f; }
+
+        charCon.Move(new Vector3(xInput * Time.deltaTime * moveSpeed, 0f, zInput * Time.deltaTime * moveSpeed));
+    }
+
+}
